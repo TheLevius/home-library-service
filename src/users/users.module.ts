60534eaefb70cd43f42ad757785c7264cb1f5f88
reflevius <reflevius@gmail.com>
2023-02-06 02:27:31 +0300

    fix: delete extra service in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from 'src/db/db.module';
-import { DbUsersTableService } from 'src/db/table.users.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
     imports: [DbModule],
     controllers: [UsersController],
-    providers: [UsersService, DbUsersTableService],
+    providers: [UsersService],
 })
 export class UsersModule {}
